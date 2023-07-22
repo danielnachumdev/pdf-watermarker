77f82b74c20760c01e7f7cee8918a56ee260e43e
danielnachumdev <danielnachumdev@gmail.com>
2023-07-22 16:41:28 +0300

    Quality of life commit

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A python command-line utitily to apply watermark to all pages of a pdf
 2. create folder for program
 3. create new venv inside folder: `virtualenv my_venv`
 4. activate venv: `.\my_venv\Scripts\activate`
-5. install dependencies: `pip install img2pdf PyPDF2`
+5. install dependencies: `pip isntall -r .\requirements.txt`
 6. ` python pdf-watermarker.py [pdf_file] ?[output_file] ?[watermark_file]`
 	* `[pdf_file]` - is the path to the pdf file on which you want to add a watermark
 	* `?[output_file]` - an optional argument which specifies the **full path** of the output file and default to `.../Desktop/output.pdf`
--- pdf-watermarker.py
+++ pdf-watermarker.py
@@ -7,7 +7,7 @@ from typing import Tuple
 from local_vars import DEFAULT_WATERMARK_PATH
 DEFAULT_PDF_INDEX = 1
 DEFAULT_OUTPUT_INDEX = 2
-DEFUALT_OUTPUT_NAME = "output.pdf"
+DEFAULT_OUTPUT_NAME = "output.pdf"
 DEFAULT_WATERMARK_INDEX = 3
 DESKTOP_PATH = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 TMP_PDF = "temporary_file_671823476598176591.pdf"
@@ -22,23 +22,20 @@ def apply_watermark(pdf_path: str, output_path: str, watermark_path: str) -> Non
     def delete_file(path: str) -> None:
         os.remove(path)
 
-    watermark_file = open(watermark_path, 'rb')
-    watermark_page = picture_to_pdf(watermark_path).getPage(0)
-    input_file = open(pdf_path, 'rb')
-    input_pdf = PyPDF2.PdfFileReader(input_file)
-    output = PyPDF2.PdfFileWriter()
-    result = open(output_path, 'wb')
-    for i in range(input_pdf.numPages):
-        print(f"Page {i+1} / {input_pdf.numPages}")
-        page = input_pdf.getPage(i)
-        page.mergePage(watermark_page,)
-        output.addPage(page)
-        output.write(result)
-    result.close()
-    watermark_file.close()
-    input_file.close()
-    delete_file(TMP_PDF)
-    print("Finished!")
+    with open(watermark_path, 'rb') as watermark_file:
+        watermark_page = picture_to_pdf(watermark_path).getPage(0)
+        with open(pdf_path, 'rb')as input_file:
+            input_pdf = PyPDF2.PdfFileReader(input_file)
+            output = PyPDF2.PdfFileWriter()
+            with open(output_path, 'wb') as result:
+                for i in range(input_pdf.numPages):
+                    print(f"Page {i+1} / {input_pdf.numPages}")
+                    page = input_pdf.getPage(i)
+                    page.mergePage(watermark_page,)
+                    output.addPage(page)
+                    output.write(result)
+                delete_file(TMP_PDF)
+                print("Finished!")
 
 
 def is_input_valid(pdf_file, output_file, watermark):
@@ -62,7 +59,7 @@ def is_input_valid(pdf_file, output_file, watermark):
 def parse_input(argv: list[str]) -> Tuple[str, str, str]:
     pdf_file = argv[DEFAULT_PDF_INDEX]
     watermark = DEFAULT_WATERMARK_PATH
-    output_file = DESKTOP_PATH+"//"+DEFUALT_OUTPUT_NAME
+    output_file = DESKTOP_PATH+"//"+DEFAULT_OUTPUT_NAME
     if len(argv) >= 3:
         watermark = argv[DEFAULT_WATERMARK_INDEX]
     if (len(argv) >= 4):
